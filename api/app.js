4675fa1fbec805bed34faa74929a996593ca8fd5
felixNyalenda <felixasejunior@gmail.com>
2024-04-19 12:59:58 +0300

    user auth build

--- api/app.js
+++ api/app.js
@@ -1,5 +1,7 @@
-import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
+import express from "express";
 
 import trustAccountRouter from "./routes/trustAccountRouter.route.js";
 import authRouter from "./routes/auth.route.js";
@@ -7,7 +9,9 @@ import authRouter from "./routes/auth.route.js";
 const app = express();
 
 dotenv.config();
+app.use(cookieParser());
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/trust-accounts", trustAccountRouter);
 app.use("/api/auth", authRouter);
