09b3ea90d064041f10596a8ee28807d109b3b536
felixAse <felixasejunior@gmail.com>
2024-04-19 10:47:00 +0300

    start auth

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import trustAccountRouter from "./routes/trustAccountRouter.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ dotenv.config();
 app.use(express.json());
 
 app.use("/api/trust-accounts", trustAccountRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(8800, () => {
   console.log("Server running!");
