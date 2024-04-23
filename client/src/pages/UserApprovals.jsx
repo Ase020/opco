5bc488dca951819bdd5739a6c9b1b0c2576b4c99
felixAse <felixasejunior@gmail.com>
2024-04-23 14:26:26 +0300

    conditonally render users

--- client/src/pages/UserApprovals.jsx
+++ client/src/pages/UserApprovals.jsx
@@ -10,19 +10,35 @@ function UserApprovals() {
       <div className="w-full flex flex-col gap-6">
         <h2 className="font-bold text-3xl">Verified Users</h2>
 
-        <ul className="px-5 flex flex-col gap-3">
-          {verifiedUsers.length > 0 &&
-            verifiedUsers.map((user) => <User key={user.id} user={user} />)}
-        </ul>
+        {verifiedUsers ? (
+          <ul className="px-5 flex flex-col gap-3">
+            {verifiedUsers.length > 0 &&
+              verifiedUsers.map((user) => <User key={user.id} user={user} />)}
+          </ul>
+        ) : (
+          <div className="flex-center border size-full">
+            <p className="font-bold text-2xl italic text-gray-500">
+              No Verified Users Found!
+            </p>
+          </div>
+        )}
       </div>
 
       <div className="w-full flex flex-col gap-6">
         <h2 className="font-bold text-3xl">Unverified Users</h2>
 
-        <ul className="px-5 flex flex-col gap-3">
-          {unverifiedUsers.length > 0 &&
-            unverifiedUsers.map((user) => <User key={user.id} user={user} />)}
-        </ul>
+        {unverifiedUsers.length > 0 ? (
+          <ul className="px-5 flex flex-col gap-3">
+            {unverifiedUsers.length > 0 &&
+              unverifiedUsers.map((user) => <User key={user.id} user={user} />)}
+          </ul>
+        ) : (
+          <div className="flex-center border size-full">
+            <p className="font-bold text-2xl italic text-gray-500">
+              No Unverified Users Found!
+            </p>
+          </div>
+        )}
       </div>
     </section>
   );
