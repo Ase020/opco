a700170ed36a1c09f528cd222c1a66d2629aa0db
felixNyalenda <felixasejunior@gmail.com>
2024-04-30 10:45:19 +0300

    add title

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,7 +15,10 @@ const Home = () => {
       : navLinks.filter((link) => link.userType === currentUser.userType);
 
   return (
-    <main className="max-w-[1440px]  mx-auto py-10">
+    <main className="max-w-[1440px] mx-auto py-10 flex flex-col gap-12">
+      <h1 className="text-center text-4xl font-bold">
+        Welcome to Airtel Data Transmission Portal
+      </h1>
       <nav className="flex items-center flex-col gap-6">
         {renderNavLinks.map(({ id, title, link }) => (
           <Link
